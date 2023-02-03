09ec4ab84e930e7fa5d42afed4a7a5b737535231
honnigam <lucasvm87@gmail.com>
2023-02-02 22:59:06 -0300

    create value

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function addFilme () {
-   alert ('clicou')
+function addFilme(){
+   var favFilm = document.getElementById('filme').value
+   var listFilm = document.getElementById('filmList')
+   
+   listFilm.innerHTML = listFilm.innerHTML + '<img src=' + favFilm + '>'
+
+   document.getElementById('filme').value = ''
 }
